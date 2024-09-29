fbbcaab30fbed5a28207b5f1359460cd2efbd2eb
Nebular_Scripter <nguyphi974@gmail.com>
2024-09-29 15:38:03 +0700

    x

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99157e7b18e7b2ee0fc8b354ab76136fe0a4bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb50ddbd75696c93bcf89acefff7498be9590a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjectManagementAPI/obj/Debug/net8.0/ProjectManagementAPI.AssemblyInfo.cs
+++ ProjectManagementAPI/obj/Debug/net8.0/ProjectManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99157e7b18e7b2ee0fc8b354ab76136fe0a4bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb50ddbd75696c93bcf89acefff7498be9590a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
